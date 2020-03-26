28f999564218ea46aaf65b9a55d35fc704413a3d
chenwengao <chenwengao@jd.com>
2020-03-26 15:28:35 +0800

    update

---
+++ bin/cli.js
@@ -0,0 +1,54 @@
+const simplebook=require('..')
+const openurl=require('openurl')
+const fs=require('fs')
+
+const args={}
+process.argv.slice(2).map(arg=>{
+  arg.replace(/^--(.+?)=(.+)$/,(_, k, v)=>{
+    args[k.toUpperCase()]=v
+  })
+})
+
+const mkdir=dir=>{
+  try{fs.mkdirSync(dir)}catch(e) {
+    if(!fs.existsSync(dir)) throw new Error('目录生成失败: '+dir)
+  }
+}
+const mkfile=(fn, str)=>{
+  try{
+    if(fs.existsSync(fn)) return;
+    fs.writeFileSync(fn, str)
+  }catch(e) {
+    if(!fs.existsSync(fn)) throw new Error('写入文件失败: '+dir)
+  }
+}
+const SVID_STR=`<?js
+const apis={}
+
+apis.readpackage=async _=>{
+  return {
+    msg: 'helo',
+    ok: !0,
+  }
+}
+
+module.exports=async svid=> {
+  return JSON.stringify(apis[svid]? await apis[svid](): {msg: svid+' 不存在', ok: !1})
+}`
+
+try{
+  const {DIR, PORT, OPEN}=args
+  if(!DIR) throw new Error('没有填写文件目录')
+  mkdir(DIR)
+  mkdir(DIR+'/md')
+  mkfile(DIR+'/svid.html', SVID_STR)
+
+  simplebook({
+    SVID_HTML: DIR+'/svid.html',
+    BOOK_DIR: DIR+'/md',
+    port: PORT,
+  }).then(url=>{
+    if(OPEN) openurl.open(url)
+    console.log('可在浏览器查看: '+url)
+  })
+}catch(e) {}
